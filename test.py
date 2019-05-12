53f9dd3213022a1537edd4f6f00c72cd7ceca720
Shubham chhimpa <chhimpa.shubh04@gmail.com>
2019-05-12 16:33:51 +0530

    seconds changed

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import VideoSegment as vs
 
-my_video_segment1 = vs.VideoSegment((73, 109, 137), 360)
+my_video_segment1 = vs.VideoSegment((73, 109, 137), 10)
 my_video_segment1.add_text_component("this is the text",list(range(5,7)),150,130)
 
 # my_video_segment1.add_image_component('./transparent/penguin.png',list(range(10,20)),100,100)
